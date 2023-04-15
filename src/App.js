82e3cf0295889a1d5a26aaadaaf95c74bf4ab9a2
iLnsiC <besswalid@gmail.com>
2023-04-15 17:05:26 +0200

    [UPDATE] APP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import Navbar from "./view/NavBar";
-
+import Navbar from "./View/Layout/NavBar";
+import Footer from "./View/Layout/Footer";
 function App() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 }
