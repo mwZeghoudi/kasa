42cf5944f400083dc02198219bd7da6725972dd5
iLnsiC <besswalid@gmail.com>
2023-04-16 15:15:58 +0200

    [FIX] Navbar

--- src/view/Layout/NavBar.js
+++ src/view/Layout/NavBar.js
@@ -3,7 +3,7 @@ import "./NavBar.css";
 import NavBarLinks from "../../Component/NavBarLinks";
 export default function Navbar(props) {
   return (
-    <nav className="nav-bar" style={{ backgroundColor: "red" }}>
+    <nav className="nav-bar">
       <div className="nav-logo">
         K
         <div className="logo" tabIndex={0}>
