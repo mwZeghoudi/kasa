a8e66e05517e5aa5c48b313bd68aded560bdf821
iLnsiC <besswalid@gmail.com>
2023-04-16 13:21:32 -0400

    [INIT] Home page

---
+++ src/View/Content/Home.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import Card from "../../Component/Card";
+import "./Home.css";
+
+export default function Home() {
+  const [housing, setHousing] = useState([]);
+  let offers = <></>;
+
+  useEffect(() => {
+    fetch(`${process.env.PUBLIC_URL}/logement-data.json`)
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error("La réponse du serveur n'est pas OK");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setHousing(data);
+      })
+      .catch((error) => {
+        console.error("Une erreur s'est produite :", error);
+      });
+  }, []);
+
+  return (
+    <div className="home-container">
+      <Card
+        rounded={10}
+        content={"Chez vous, partout et ailleurs"}
+        isBanner={true}
+        image={
+          "https://img.freepik.com/photos-gratuite/jetee-au-bord-lac-hallstatt-autriche_181624-44201.jpg"
+        }
+      />
+      <Card rounded={25} isGrey={true}>
+        {housing.map((e) => (
+          <Card
+            key={e.id}
+            id={e.id}
+            rounded={10}
+            gradient={true}
+            content={e.title}
+            isLocation={true}
+            image={e.cover}
+            data={e}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+}
