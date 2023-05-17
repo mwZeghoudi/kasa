a93c15ad4324ed57fe8599e4d0108d45752d461c
Mohammed-Walid Zeghoudi <mwzeghoudi@santeformapro.com>
2023-05-17 15:49:39 +0200

    [WIP]

--- src/Component/Carrousel.js
+++ src/Component/Carrousel.js
@@ -14,6 +14,7 @@ export default function Carrousel(props) {
   const contentRefs = useRef(null);
   const [activeIndex, setActiveIndex] = useState(1);
   const [dataLoaded, setDataLoaded] = useState(false);
+  const [isAlreadyLoaded, setIsAlreadyLoaded] = useState(false);
   const [picturesElement, setPicturesElement] = useState(null);
   const [isCarousselDisabled, setIsCarousselDisabled] = useState(false);
   const [pictureWidth, setPictureWidth] = useState(0);
@@ -26,6 +27,7 @@ export default function Carrousel(props) {
             key={i}
             className={"carrousel-picture"}
             data-index={i + 1}
+            data-active={i === 0 ? true : false}
             src={e}
           />
         ))}
@@ -44,116 +46,44 @@ export default function Carrousel(props) {
     setPictureWidth(contentRefs.current.offsetWidth);
   };
 
-  useEffect(() => {
-    console.log(pictureWidth);
-    if (dataLoaded) {
+  const updateClone = () =>{
+    if (isAlreadyLoaded === false) {
       Array.from(contentRefs.current.children).forEach((e, i) => {
-        // console.log(e);
-        e.style.left = `${(pictureWidth * i) + 1}px`;
+        e.style.left = `${(pictureWidth * i)}px`;
       });
+      const clonedElement = contentRefs.current.lastChild.cloneNode(true);
+      contentRefs.current.insertBefore(clonedElement, contentRefs.current.firstChild);
+      contentRefs.current.firstChild.style.left = `${-pictureWidth}px`;
+      setIsAlreadyLoaded(true)
+    }
+
+  }
+
+  useEffect(() => {
+    if (dataLoaded && isAlreadyLoaded === false) {
+      updateClone()
     }
   }, [dataLoaded]);
 
-  // const scrollPicture = (isNext) => {
-  //   //var part
-  //   const pic = document.getElementById("carrousel-picture");
-  //   setIsCarousselDisabled(true);
-  //   let newAdjacentElement = [];
-  //   const activeElIndex = parseInt(activeElement.dataset.index);
-  //   let nextElement = contentRefs.current.find(
-  //     (element) => element.dataset.index == activeElIndex + 1
-  //   );
-  //   let prevElement = contentRefs.current.find(
-  //     (element) => element.dataset.index == activeElIndex - 1
-  //   );
-  //   if (activeElIndex >= pictures.length) {
-  //     nextElement = contentRefs.current.find(
-  //       (element) => element.dataset.index == 1
-  //     );
-  //   } else if (activeElIndex <= 1) {
-  //     prevElement = contentRefs.current.find(
-  //       (element) => element.dataset.index == pictures.length
-  //     );
-  //   }
-  //   if (pictures.length > 3) {
-  //     if (activeElIndex >= pictures.length) {
-  //       newAdjacentElement[1] = contentRefs.current.find(
-  //         (element) => element.dataset.index == 2
-  //       );
-  //     } else if (activeElIndex >= pictures.length - 1) {
-  //       newAdjacentElement[1] = contentRefs.current.find(
-  //         (element) => element.dataset.index == 1
-  //       );
-  //     } else if (activeElIndex <= 1) {
-  //       newAdjacentElement[0] = contentRefs.current.find(
-  //         (element) => element.dataset.index == pictures.length - 1
-  //       );
-  //     } else if (activeElIndex <= 2) {
-  //       newAdjacentElement[0] = contentRefs.current.find(
-  //         (element) => element.dataset.index == pictures.length
-  //       );
-  //     }
-  //   }
+  const scrollPicture = (isNext) => {
+    const children = Array.from(contentRefs.current.children);
+    const oldPicture = children.find(child => child.dataset.active === "true");
+    let nextPicture;
+    const indexOldPicture = children.indexOf(oldPicture);
 
-  //   // Animation part
-  //   activeElement.classList.remove("active");
-  //   if (isNext) {
-  //     nextElement.classList.remove("carrousel-left");
-  //     nextElement.classList.add("active");
-  //     if (!activeElement.classList.contains("carrousel-left")) {
-  //       activeElement.classList.add("carrousel-left");
-  //     }
-  //     setActiveElement(nextElement);
-  //     setActiveIndex(nextElement.dataset.index);
+    if (isNext) {
+      nextPicture = children[indexOldPicture + 1];
+    }else{
+      nextPicture = children[indexOldPicture - 1];
+    }
+    oldPicture.dataset.active = 'false';
+    nextPicture.dataset.active = 'true';
 
-  //     if (pictures.length === 2) {
-  //     } else if (pictures.length === 3) {
-  //     } else if (pictures.length > 3) {
-  //       if (
-  //         !(activeElIndex >= pictures.length - 1) &&
-  //         !(activeElIndex >= pictures.length)
-  //       ) {
-  //         newAdjacentElement[1] = contentRefs.current.find(
-  //           (element) => element.dataset.index == activeElIndex + 2
-  //         );
-  //       }
-  //       if (newAdjacentElement[1].classList.contains("carrousel-left")) {
-  //         newAdjacentElement[1].classList.remove("carrousel-left");
-  //       }
-  //     }
-  //   } else {
-  //     prevElement.classList.add("active");
-  //     prevElement.classList.remove("carrousel-left");
-  //     if (activeElement.classList.contains("carrousel-left")) {
-  //       activeElement.classList.remove("carrousel-left");
-  //     }
-  //     setActiveElement(prevElement);
-  //     setActiveIndex(prevElement.dataset.index);
-
-  //     if (pictures.length === 2) {
-  //     } else if (pictures.length === 3) {
-  //     } else if (pictures.length > 3) {
-  //       if (!(activeElIndex <= 1) && !(activeElIndex <= 2)) {
-  //         newAdjacentElement[0] = contentRefs.current.find(
-  //           (element) => element.dataset.index == activeElIndex - 2
-  //         );
-  //       }
-  //       if (!newAdjacentElement[0].classList.contains("carrousel-left")) {
-  //         newAdjacentElement[0].classList.add("carrousel-left");
-  //       }
-  //     }
-  //   }
-
-  //   if (pictures.length === 2) {
-  //     setTimeout(function () {
-  //       activeElement.classList.toggle("carrousel-left");
-  //     }, 500);
-  //   }
-
-  //   return new Promise(function (resolve) {
-  //     setTimeout(resolve, 800);
-  //   });
-  // };
+    console.log(children);
+    console.log(oldPicture);
+    console.log(nextPicture);
+    console.log(indexOldPicture);
+  };
 
   return (
     <div className="carrousel">
@@ -162,12 +92,9 @@ export default function Carrousel(props) {
           <button
             id="carrousel-chevron-left"
             disabled={isCarousselDisabled}
-            // onClick={function () {
-            //   setIsCarousselDisabled(true);
-            //   scrollPicture(false).then((value) => {
-            //     setIsCarousselDisabled(false);
-            //   });
-            // }}
+            onClick={function(){
+              scrollPicture(false)
+            }}
           >
             <FontAwesomeIcon icon="chevron-left" />
           </button>
@@ -177,12 +104,9 @@ export default function Carrousel(props) {
           <button
             id="carrousel-chevron-right"
             disabled={isCarousselDisabled}
-            // onClick={function () {
-            //   setIsCarousselDisabled(true);
-            //   scrollPicture(true).then((value) => {
-            //     setIsCarousselDisabled(false);
-            //   });
-            // }}
+            onClick={function(){
+              scrollPicture(true)
+            }}
           >
             <FontAwesomeIcon icon="chevron-right" />
           </button>
