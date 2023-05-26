2122849ff19db870e2c4d97994e0128c8b8eccf9
iLnsiC <besswalid@gmail.com>
2023-05-26 15:59:04 +0200

    [FIX] Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import Routing from "./View/Layout/Routing";
 import Header from "./View/Layout/Header";
 import Footer from "./View/Layout/Footer";
-import NotFoundPage from "./View/Layout/NotFoundPage";
-import Home from "./View/Content/Home";
-import About from "./View/Content/About";
-import Housing from "./View/Content/Housing";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-
-      <Routes>
-        <Route path="/kasa" element={<Home />} />
-        <Route path="/kasa/about" element={<About />} />
-        <Route path="/kasa/housing/:productId" element={<Housing />} />
-        <Route path="/kasa/*" element={<NotFoundPage />} />
-        <Route path="/kasa/not-found" element={<NotFoundPage />} />
-      </Routes>
+      <Routing />
       <Footer />
     </BrowserRouter>
   );
